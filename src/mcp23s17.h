04a23c9e87bb854f56e2ff4ec4168120547f4e52
Thomas Preston <thomasmarkpreston@gmail.com>
2013-11-21 13:07:13 +0000

    added copyright at top of header file

--- src/mcp23s17.h
+++ src/mcp23s17.h
@@ -1,7 +1,22 @@
 /* mcp23s17.h
  * A simple static library for controlling an MCP23S17 port expander over SPI.
  * Datasheet: http://ww1.microchip.com/downloads/en/devicedoc/21952b.pdf
- */
+ *
+ * Copyright (C) 2013 Thomas Preston <thomas.preston@openlx.org.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef _MCP23S17_H
 #define _MCP23S17_H
 
