f0f65c5052c05b1b513b7576ca6907cb777e4116
Thomas Preston <thomasmarkpreston@gmail.com>
2014-03-07 10:43:24 +0000

    updated docs to latest version

--- docs/html/search/all_6d.js
+++ docs/html/search/all_6d.js
@@ -1,9 +1,12 @@
 var searchData=
 [
   ['mcp23s17_2eh',['mcp23s17.h',['../mcp23s17_8h.html',1,'']]],
+  ['mcp23s17_5fdisable_5finterrupts',['mcp23s17_disable_interrupts',['../mcp23s17_8h.html#ab4c8197ef196c6490270684b85883f33',1,'mcp23s17.c']]],
+  ['mcp23s17_5fenable_5finterrupts',['mcp23s17_enable_interrupts',['../mcp23s17_8h.html#aac4ee3153a806f58ebf49b3fa83a4ea8',1,'mcp23s17.c']]],
   ['mcp23s17_5fopen',['mcp23s17_open',['../mcp23s17_8h.html#acb543fc291843b8d2c8316e20b01400a',1,'mcp23s17.c']]],
   ['mcp23s17_5fread_5fbit',['mcp23s17_read_bit',['../mcp23s17_8h.html#a93b365f10c1ca633945dfcece0e8c1fa',1,'mcp23s17.c']]],
   ['mcp23s17_5fread_5freg',['mcp23s17_read_reg',['../mcp23s17_8h.html#a57b4cceeb9053f3f579a9acb0ed7cc7e',1,'mcp23s17.c']]],
+  ['mcp23s17_5fwait_5ffor_5finterrupt',['mcp23s17_wait_for_interrupt',['../mcp23s17_8h.html#aafb367db1292b7fdd6002b7246d3613e',1,'mcp23s17.c']]],
   ['mcp23s17_5fwrite_5fbit',['mcp23s17_write_bit',['../mcp23s17_8h.html#a68d9c3c6577673cb30ff5c0baa85f4dc',1,'mcp23s17.c']]],
   ['mcp23s17_5fwrite_5freg',['mcp23s17_write_reg',['../mcp23s17_8h.html#aa3586d18201da61607afed24f3b8a9d7',1,'mcp23s17.c']]]
 ];
